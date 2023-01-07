41ea2a751540e4dd41dbe50f03cc2caf441df426
Gouterevathi <revathigoute0407@gmail.com>
2023-01-07 14:41:21 +1000

    This is first program

---
+++ src/github_test/Git_Test2.java
@@ -0,0 +1,12 @@
+package github_test;
+
+public class Git_Test2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("practiced--2nd test is done");
+	}
+
+	
+	
+}
---
+++ src/github_test/Git_test1.java
@@ -0,0 +1,10 @@
+package github_test;
+
+public class Git_test1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("test--1 is printed");
+	}
+
+}
